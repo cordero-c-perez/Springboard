1b01f825127815830d94b1d3b114a5df429b9c6b
cordero-c-perez <cordero.c.perez@gmail.com>
2021-02-03 16:34:15 -0500

    readability changes

--- SQLCaseStudy/SQLTasks Tier 2.-completedsql.sql
+++ SQLCaseStudy/SQLTasks Tier 2.-completedsql.sql
@@ -35,16 +35,16 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
-/*SELECT name
+SELECT name
 FROM Facilities
 WHERE membercost IS NOT NULL
-AND membercost != 0*/
+AND membercost != 0
 
 /* Q2: How many facilities do not charge a fee to members? */
 
-/*SELECT COUNT(name)
+SELECT COUNT(name)
 FROM Facilities
-WHERE membercost = 0*/
+WHERE membercost = 0
 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
@@ -52,43 +52,43 @@ where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
-/*SELECT facid, name, membercost, monthlymaintenance
+SELECT facid, name, membercost, monthlymaintenance
 FROM Facilities
-WHERE membercost < .20 * monthlymaintenance*/
+WHERE membercost < .20 * monthlymaintenance
 
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 
-/*SELECT *
+SELECT *
 FROM Facilities
-WHERE facid IN (1,5)*/
+WHERE facid IN (1,5)
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
-/*SELECT name, 
+SELECT name, 
 	CASE WHEN monthlymaintenance <= 100 THEN 'cheap'
 		WHEN monthlymaintenance > 100 THEN 'expensive'
 		ELSE NULL END AS label
-FROM Facilities*/
+FROM Facilities
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
-/*SELECT firstname, surname, joindate
+SELECT firstname, surname, joindate
 FROM Members
 WHERE joindate = (SELECT MAX(joindate) FROM Members)
-ORDER BY joindate DESC*/
+ORDER BY joindate DESC
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
-/*SELECT DISTINCT CONCAT(m.surname,', ', m.firstname,' - ', fac.name)
+SELECT DISTINCT CONCAT(m.surname,', ', m.firstname,' - ', fac.name)
 FROM Bookings AS b
 LEFT JOIN Members AS m
 ON b.memid = m.memid
@@ -99,7 +99,7 @@ ON b.facid = fac.facid
 WHERE b.facid IN (SELECT facid
 					FROM Facilities
 					WHERE name LIKE '%tennis%court%')
-ORDER BY m.surname*/
+ORDER BY m.surname
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -108,7 +108,7 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
-/*SELECT f.name AS facility, CONCAT(m.surname,', ', m.firstname) AS name,
+SELECT f.name AS facility, CONCAT(m.surname,', ', m.firstname) AS name,
 	CASE WHEN b.memid != 0 THEN b.slots*f.membercost
 		WHEN b.memid = 0 THEN b.slots*f.guestcost
 		ELSE NULL END AS total_cost
@@ -120,11 +120,11 @@ ON b.memid = m.memid
 WHERE b.starttime BETWEEN '2012-09-14 00:00:00' AND '2012-09-14 23:59:59'
 AND (CASE WHEN b.memid != 0 THEN b.slots*f.membercost
 		WHEN b.memid = 0 THEN b.slots*f.guestcost
-		ELSE NULL END) > 30*/
+		ELSE NULL END) > 30
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
-/*SELECT f.name AS facility, CONCAT(m.surname,', ', m.firstname) AS name,
+SELECT f.name AS facility, CONCAT(m.surname,', ', m.firstname) AS name,
 	CASE WHEN b.memid != 0 THEN b.slots*f.membercost
 		WHEN b.memid = 0 THEN b.slots*f.guestcost
 		ELSE NULL END AS total_cost
@@ -136,7 +136,7 @@ ON b.memid = m.memid
 WHERE b.starttime BETWEEN '2012-09-14 00:00:00' AND '2012-09-14 23:59:59'
 AND (CASE WHEN b.memid != 0 THEN b.slots*f.membercost
 		WHEN b.memid = 0 THEN b.slots*f.guestcost
-		ELSE NULL END) > 30*/
+		ELSE NULL END) > 30
 
 /* PART 2: SQLite
 
